70145d63cab76d9aab87abe22e18266dc16c116b
ImnMuqri <imnmuqri@gmail.com>
2026-03-14 00:50:05 +0800

    Fix localhost

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
     public: {
-      apiBase: 'http://localhost:3001'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE
     }
   }
 })
